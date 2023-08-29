f1c320e265a737d807a8e25ce718591a04679a9d
Zahid Islam <zahidcseku@gmail.com>
2023-08-29 19:25:24 +1000

    publications added

--- pages/3_📚_Publications.py
+++ pages/3_📚_Publications.py
@@ -4,9 +4,27 @@ st.title("Publications")
 
 st.subheader("2021", divider="grey")
 st.markdown("""
-- Zobair, K. M., Sanzogni, L., Houghton, L., & **Islam, M. Z.** (2021). __Forecasting care seekers satisfaction 
-with telemedicine using machine learning and structural equation modeling__. PloS one, 16(9), e0257300.
-- Zobair, K. M., Sanzogni, L., Houghton, L., & **Islam, M. Z.** (2022). __Combining Deep Neural Network and PLS-SEM 
-to Predict Patients’ Continuity with Telemedicine__. International Journal of Information Technology & Decision 
+- Zobair, K. M., Sanzogni, L., Houghton, L., & **Islam, M. Z.** (2021). _Forecasting care seekers satisfaction 
+with telemedicine using machine learning and structural equation modeling_. PloS one, 16(9), e0257300.
+- Zobair, K. M., Sanzogni, L., Houghton, L., & **Islam, M. Z.** (2022). _Combining Deep Neural Network and PLS-SEM 
+to Predict Patients’ Continuity with Telemedicine_. International Journal of Information Technology & Decision 
 Making, 21(05), 1555-1589.
+- **Islam, M. Z.**, Liu, J., Li, J., Liu, L., & Kang, W. (2020, July). _Evidence weighted tree ensembles for text 
+classification_. In Proceedings of the 43rd International ACM SIGIR Conference on Research and Development 
+in Information Retrieval (pp. 1737-1740).
+- **Islam, M. Z.**, Liu, J., Li, J., Liu, L., & Kang, W. (2019, November). _A semantics aware random forest for text 
+classification_. In Proceedings of the 28th ACM international conference on information and knowledge 
+management (pp. 1061-1070).
+- **Islam, M. Z.**, Liu, J., Liu, L., Li, J., & Kang, W. (2019). _Semantic explanations in ensemble learning_. In 
+Advances in Knowledge Discovery and Data Mining: 23rd Pacific-Asia Conference, PAKDD 2019, 
+Macau, China, April 14-17, 2019, Proceedings, Part I 23 (pp. 29-41). Springer International Publishing.
+- Rahaman, G. A., & **Islam, M. Z.** (2016). _Color transform analysis for microscale image segmentation to study 
+halftone model parameters_. Open Computer Science, 6(1), 148-167.
+- Ferdous, R., **Islam, M. Z.**, & Ali, F. B. (2016, May). _Identifying miRNA-mRNA interactions by a combination of 
+spearman's rank correlation and IDA_. In 2016 5th International Conference on Informatics, Electronics and 
+Vision (ICIEV) (pp. 903-908). IEEE.
+- Kadir, M., Sobhan, S., & **Islam, M. Z.** (2016, May). _Temporal relation extraction using Apriori algorithm_. 
+In 2016 5th International Conference on Informatics, Electronics and Vision (ICIEV) (pp. 915-920). IEEE.
+- Habiba, M., Islam, M. R., Ali, A. S., & **Islam, M. Z.** (2016). _A new approach to access control in cloud_. 
+Arabian Journal for Science and Engineering, 41, 1015-1030.
 """)
