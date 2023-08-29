8febfe820d909e4434a77b44ba39af48bf1439b4
Zahid Islam <zahidcseku@gmail.com>
2023-08-29 19:37:57 +1000

    all publications added

--- pages/3_📚_Publications.py
+++ pages/3_📚_Publications.py
@@ -2,22 +2,38 @@ import streamlit as st
 
 st.title("Publications")
 
+st.subheader("2022", divider="grey")
+st.markdown("""
+- Zobair, K. M., Sanzogni, L., Houghton, L., & **Islam, M. Z.** (2022). _Combining Deep Neural Network and PLS-SEM 
+to Predict Patients’ Continuity with Telemedicine_. International Journal of Information Technology & Decision 
+Making, 21(05), 1555-1589.
+""")
+
 st.subheader("2021", divider="grey")
 st.markdown("""
 - Zobair, K. M., Sanzogni, L., Houghton, L., & **Islam, M. Z.** (2021). _Forecasting care seekers satisfaction 
 with telemedicine using machine learning and structural equation modeling_. PloS one, 16(9), e0257300.
-- Zobair, K. M., Sanzogni, L., Houghton, L., & **Islam, M. Z.** (2022). _Combining Deep Neural Network and PLS-SEM 
-to Predict Patients’ Continuity with Telemedicine_. International Journal of Information Technology & Decision 
-Making, 21(05), 1555-1589.
+""")
+
+st.subheader("2020", divider="grey")
+st.markdown("""
 - **Islam, M. Z.**, Liu, J., Li, J., Liu, L., & Kang, W. (2020, July). _Evidence weighted tree ensembles for text 
 classification_. In Proceedings of the 43rd International ACM SIGIR Conference on Research and Development 
 in Information Retrieval (pp. 1737-1740).
+""")
+
+st.subheader("2019", divider="grey")
+st.markdown("""
 - **Islam, M. Z.**, Liu, J., Li, J., Liu, L., & Kang, W. (2019, November). _A semantics aware random forest for text 
 classification_. In Proceedings of the 28th ACM international conference on information and knowledge 
 management (pp. 1061-1070).
 - **Islam, M. Z.**, Liu, J., Liu, L., Li, J., & Kang, W. (2019). _Semantic explanations in ensemble learning_. In 
 Advances in Knowledge Discovery and Data Mining: 23rd Pacific-Asia Conference, PAKDD 2019, 
 Macau, China, April 14-17, 2019, Proceedings, Part I 23 (pp. 29-41). Springer International Publishing.
+""")
+
+st.subheader("2016", divider="grey")
+st.markdown("""
 - Rahaman, G. A., & **Islam, M. Z.** (2016). _Color transform analysis for microscale image segmentation to study 
 halftone model parameters_. Open Computer Science, 6(1), 148-167.
 - Ferdous, R., **Islam, M. Z.**, & Ali, F. B. (2016, May). _Identifying miRNA-mRNA interactions by a combination of 
@@ -27,4 +43,43 @@ Vision (ICIEV) (pp. 903-908). IEEE.
 In 2016 5th International Conference on Informatics, Electronics and Vision (ICIEV) (pp. 915-920). IEEE.
 - Habiba, M., Islam, M. R., Ali, A. S., & **Islam, M. Z.** (2016). _A new approach to access control in cloud_. 
 Arabian Journal for Science and Engineering, 41, 1015-1030.
+""")
+
+
+st.subheader("2014", divider="grey")
+st.markdown("""
+- **Islam, M. Z.**, & Mondal, A. K. (2014, December). _Towards a standard bangla photoocr: Text detection and 
+localization_. In 2014 17th International Conference on Computer and Information Technology 
+(ICCIT) (pp. 198-203). IEEE.
+""")
+
+
+st.subheader("2012", divider="grey")
+st.markdown("""
+- **Islam, M. Z.**, & MacCaull, W. (2012). _A One-Pass Tableau-Based Workflow Verification Framework_. In 
+PAAR@ IJCAR (pp. 58-71).
+- **Islam, M. Z.**, Mashiyat, A. S., Khan, K. N., & Karim, S. M. (2012). _A Tableau Based Automated Theorem 
+Prover Using High Performance Computing_. J. Comput., 7(3), 597-607.
+- **Islam, M. Z.** (2012). _A tableau-based workflow verification framework for computation Tree Logic (CTL)_
+(Master dissertation, St. Francis Xavier University).
+""")
+
+
+st.subheader("2010", divider="grey")
+st.markdown("""
+- **Islam, M. Z.**, Mashiyat, A. S., Khan, K. N., & Karim, S. M. (2010, December). 
+_Towards a tableau based high performance automated theorem prover_. In 2010 13th International Conference 
+on Computer and Information Technology (ICCIT) (pp. 406-411). IEEE.
+""")
+
+st.subheader("2008", divider="grey")
+st.markdown("""
+- Islam, M. R., **Islam, M. Z.**, & Leyla, N. (2008). _A tree‐based approach to matchmaking algorithms for 
+resource discovery_. International Journal of Network Management, 18(5), 427-436.
+""")
+
+st.subheader("2007", divider="grey")
+st.markdown("""
+- Islam, R., **Islam, Z.**, & Leyla, N. (2007, March). _A matchmaking algorithm for resource discovery on grid_. 
+In 2007 International Conference on Information and Communication Technology (pp. 193-196). IEEE.
 """)
