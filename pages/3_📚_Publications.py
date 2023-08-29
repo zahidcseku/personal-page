665566737e9df312abe4dcdfad5c428c111eb043
Zahid Islam <zahidcseku@gmail.com>
2023-08-29 19:12:56 +1000

    publications added

--- pages/3_📚_Publications.py
+++ pages/3_📚_Publications.py
@@ -1,3 +1,12 @@
 import streamlit as st
 
-st.title("Publication")
+st.title("Publications")
+
+st.subheader("2021", divider="grey")
+st.markdown("""
+- Zobair, K. M., Sanzogni, L., Houghton, L., & **Islam, M. Z.** (2021). __Forecasting care seekers satisfaction 
+with telemedicine using machine learning and structural equation modeling__. PloS one, 16(9), e0257300.
+- Zobair, K. M., Sanzogni, L., Houghton, L., & **Islam, M. Z.** (2022). __Combining Deep Neural Network and PLS-SEM 
+to Predict Patients’ Continuity with Telemedicine__. International Journal of Information Technology & Decision 
+Making, 21(05), 1555-1589.
+""")
