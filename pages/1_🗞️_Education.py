3586fc85cfa3249a4e1d80b9a3574e9527e92f01
Zahid Islam <zahidcseku@gmail.com>
2023-08-29 17:58:27 +1000

    updated educatiob

--- pages/1_🗞️_Education.py
+++ pages/1_🗞️_Education.py
@@ -1,3 +1,22 @@
 import streamlit as st
 
-st.header("Education")
+st.title("Education")
+
+st.subheader("Post doctoral Research Fellow - Machine Learning and AI Future Science Platform", divider="grey")
+st.markdown("**Commonwealth Scientific and Industrial Research Organisation (CSIRO), Australia**")
+st.markdown("**Nov. 2020 - present**")
+
+st.markdown("""
+- Spearheaded research projects that integrate advanced machine learning techniques into practical applications.
+- Developed an innovative spatiotemporal prediction model combining recurrent neural networks (RNNs) and graph neural 
+networks (GNNs).
+- Developed explainability methods to draw insights from complex neural architectures.
+- Three research papers are in preparation for top-tier conferences/journals.
+- Interruptions: parental leave from 10/05/2022 to 06/06/2022.
+- Main research projects:
+    - Advanced deep neural network modelling involving graph neural network, recurrent neural network and 
+    zero-inflated Poisson distribution for pest abundance estimation from sparse spatiotemporal observations.
+    - Development of machine learning based estimation model for conservation actions.
+    - Addressing bias and fairness in machine learning in collaboration with social scientists. 
+"""
+            )
