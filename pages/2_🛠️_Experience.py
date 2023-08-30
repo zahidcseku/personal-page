a797fcd6f7800b8cff3402f4dff5596ee7205017
Zahid Islam <zahidcseku@gmail.com>
2023-08-31 09:18:14 +1000

    updated work experience

--- pages/2_🛠️_Experience.py
+++ pages/2_🛠️_Experience.py
@@ -1,3 +1,58 @@
 import streamlit as st
 
 st.title("Work Experience")
+
+st.header("Industry Experience")
+
+st.subheader("Web Developer | Software Engineer | Senior Software Engineer", divider="grey")
+st.markdown("**Dot Net Explorer Ltd. (Remote Job), Khulna, Bangladesh**")
+st.markdown("**Apr. 2007 - Mar. 2016**")
+
+st.markdown("""
+- Led project estimation, tool selection, and technical planning to ensure timely project delivery.
+- Collaborated with clients and Project Managers to refine graphic designs and provide technical solutions.
+- Developed web-based applications aligned with user requirements and specifications.
+- Produced technical documentation and operational procedures, facilitating effective project execution. 
+"""
+)
+
+st.subheader("Management Information System (MIS) Expert", divider="grey")
+st.markdown("**European Union Technical Assistance, Khulna, Bangladesh**")
+st.markdown("**Dec. 2014 - Apr. 2015**")
+
+st.markdown("""
+- Contributed to the ``Sundarbans Environmental And Livelihoods Security (SEALS)'' project in collaboration 
+with the European Union.
+- Developed the Sundarbans Management Information System (SMIST) to meet the requirements of the 
+Bangladesh Forest Department.
+- Produced customized reports using SMIST data to fulfil the needs of Forest officials.
+"""
+)
+
+
+st.header("Academic Experience")
+
+st.subheader("Research Assistant and PhD Candidate", divider="grey")
+st.markdown("**Data Analytics Group (currently known as 4LLab), UniSA, Australia**")
+st.markdown("**Aug. 2016 - Nov. 2020**")
+
+st.markdown("""
+- Defining, designing and executing research project scope and technical priorities. 
+- Finding research gaps in the existing literature regarding ensemble methods and addressing those 
+gaps using innovative mathematical techniques.
+- Writing high quality, high level and detailed architectures of machine learning based models.
+- Demonstrated exceptional written and communication skills and the ability to present insights in simple terms.
+"""
+)
+
+
+st.subheader("Online tutor", divider="grey")
+st.markdown("**UniSA Online, Australia**")
+st.markdown("**Jan. 2019 - Nov. 2020**")
+
+st.markdown("""
+- Helping students to understand concepts through online forums, emials and online drop-in sessions.
+- Conducting regualr drop-in sessions on critical topics.
+- Marking students assessments and exams.
+"""
+)
